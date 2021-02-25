e0f860683ff1ef5ffd478cb37055d2673ca1f171
David Wasserman <davidphilipwasserman@gmail.com>
2021-02-25 18:12:49 -0500

    Moved code to config

--- ames/data_cleaning.py
+++ ames/data_cleaning.py
@@ -1,16 +1,9 @@
 import numpy as np
-import os
 import pandas as pd
-from pathlib import Path
 
-dataPath = Path('..') / 'data'
-if not os.path.exists(dataPath):
-    os.mkdir(dataPath)
+from ames import config
 
-if not os.file_exists(dataPath / 'Ames_Housing_Price_Data.csv'):
-    raise FileError('File does not exist! Please place in the data directory.')
-
-prices = pd.read_csv(dataPath / 'Ames_Housing_Price_Data.csv')
+prices = pd.read_csv(config.HOUSING_CSV)
 #real_estate = pd.read_csv(dataPath / 'Ames_Real_Estate_Data.csv', low_memory=False)
 
 # Drop index column
@@ -19,5 +12,8 @@ prices.drop('Unnamed: 0',axis=1, inplace=True)
 # Drop duplicate record(s)
 prices = prices.loc[~prices.duplicated(),:]
 
+# TODO: Additional cleaning
+
+
 # Save to a Pickle file for ease of transport
-prices.to_pickle(dataPath / 'housing_data.pickle')
+prices.to_pickle(config.HOUSING_PICKLE)
