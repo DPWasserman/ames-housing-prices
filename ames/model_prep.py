b477a58bc4a84f632ab622d050db41c71d54ae4d
David Wasserman <davidphilipwasserman@gmail.com>
2021-02-25 18:12:58 -0500

    Initial version

---
+++ ames/model_prep.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from ames import config
+
+housing_df = pd.read_pickle(config.HOUSING_PICKLE)
+
+X = housing_df.drop('SalePrice')
+y = housing_df.loc[:, 'SalePrice']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=20, random_state=42)
