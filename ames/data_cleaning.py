0c5c4b7482098b51977256f201a8ad55089f254c
David Wasserman <davidphilipwasserman@gmail.com>
2021-02-26 13:07:15 -0500

    Cleaned some features with NAs to have zeros

--- ames/data_cleaning.py
+++ ames/data_cleaning.py
@@ -3,17 +3,22 @@ import pandas as pd
 
 from ames import config
 
-prices = pd.read_csv(config.HOUSING_CSV)
+housing = pd.read_csv(config.HOUSING_CSV)
 #real_estate = pd.read_csv(dataPath / 'Ames_Real_Estate_Data.csv', low_memory=False)
 
 # Drop index column
-prices.drop('Unnamed: 0',axis=1, inplace=True)
+housing.drop('Unnamed: 0',axis=1, inplace=True)
 
 # Drop duplicate record(s)
-prices = prices.loc[~prices.duplicated(),:]
+housing = housing.loc[~housing.duplicated(),:]
+
+# Impute NAs with zeros
+housing['TotalBsmtSF'].fillna(0, inplace=True)
+housing['BsmtUnfSF'].fillna(0, inplace=True)
+housing['GarageArea'].fillna(0,inplace=True)
 
 # TODO: Additional cleaning
 
 
 # Save to a Pickle file for ease of transport
-prices.to_pickle(config.HOUSING_PICKLE)
+housing.to_pickle(config.HOUSING_PICKLE)
