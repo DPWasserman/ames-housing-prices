5c8bee16a557b3454b459ac276fa74d5de858ce5
David Wasserman <davidphilipwasserman@gmail.com>
2021-02-26 16:06:51 -0500

    Code consolidation

---
+++ ames/data_prep.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+
+from ames import config
+
+def clean(housing_location, save_to_disk=True, output_location=config.HOUSING_PICKLE):
+
+    housing = pd.read_csv(housing_location)
+
+    # Drop index column
+    housing.drop('Unnamed: 0',axis=1, inplace=True)
+
+    # Drop duplicate record(s)
+    housing = housing.loc[~housing.duplicated(),:]
+
+    # Impute NAs with zeros
+    housing['TotalBsmtSF'].fillna(0, inplace=True)
+    housing['BsmtUnfSF'].fillna(0, inplace=True)
+    housing['GarageArea'].fillna(0,inplace=True)
+
+    # TODO: Additional cleaning
+
+    if save_to_disk:
+        # Save to a Pickle file for ease of transport
+        housing.to_pickle(output_location)
+
+    return housing
+
+
+def add_features(housing, save_to_disk=True, output_location=config.HOUSING_PICKLE):
+    """
+    This function will add features to the housing dataframe.
+    Input: housing (pandas dataframe)
+    Output: pandas dataframe
+    """
+
+    housing['TotalLivingArea'] = housing['GrLivArea'] + housing['TotalBsmtSF'] - housing['BsmtUnfSF']
+    housing['UnusedLotSize'] = housing['LotArea'] - housing['1stFlrSF']
+
+    housing['HasPool'] = (housing['PoolArea']>0)
+    
+
+    if save_to_disk:
+        # Save to a Pickle file for ease of transport
+        housing.to_pickle(output_location)
+
+    return housing
