c7454baf34f688bc4aaf8abdde6261e80579d2ee
David Wasserman <davidphilipwasserman@gmail.com>
2021-02-28 09:23:31 -0500

    Impute 0 for all continuous variables with NAs

--- ames/data_prep.py
+++ ames/data_prep.py
@@ -16,10 +16,17 @@ def clean(housing_location, save_to_disk=True, output_location=config.HOUSING_PI
     housing = housing.loc[~housing.duplicated(),:]
 
     # Impute NAs with zeros (Continuous Variable)
-    housing['TotalBsmtSF'].fillna(0, inplace=True)
-    housing['BsmtUnfSF'].fillna(0, inplace=True)
-    housing['GarageArea'].fillna(0, inplace=True)
-    housing['LotFrontage'].fillna(0, inplace=True)
+    continuous_var = ['BsmtFinSF1','BsmtFinSF2','BsmtFullBath','BsmtHalfBath',
+                    'BsmtUnfSF','GarageArea','GarageCars','GarageYrBlt',
+                    'LotFrontage','MasVnrArea','TotalBsmtSF']
+    for var in continuous_var:
+        housing[var].fillna(0, inplace=True)
+
+    # housing.loc[:, continuous_var].fillna(0, inplace=True)
+    # housing['TotalBsmtSF'].fillna(0, inplace=True)
+    # housing['BsmtUnfSF'].fillna(0, inplace=True)
+    # housing['GarageArea'].fillna(0, inplace=True)
+    # housing['LotFrontage'].fillna(0, inplace=True)
 
     # Impute NAs with None (Qualitative Variable)
     housing['BsmtQual'].fillna('None', inplace=True)
@@ -44,8 +51,9 @@ def add_features(housing, save_to_disk=True, output_location=config.HOUSING_PICK
     housing['UnusedLotSize'] = housing['LotArea'] - housing['1stFlrSF']
 
     housing['HasPool'] = (housing['PoolArea']>0)
-    
-    housing['Toilets'] = housing['HalfBath'] + housing['FullBath']
+
+    # NOTE: Should we include Basement Bathrooms?
+    housing['Toilets'] = housing['HalfBath'] + housing['FullBath'] 
     housing['Showers'] = housing['FullBath']
     housing['DecadeBuilt'] = housing['YearBuilt'].apply(lambda x: (x//10 * 10))
     housing['DecadeRemodel'] = housing['YearBuilt'].apply(lambda x: (x//10 * 10))
