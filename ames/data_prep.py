de0034d902efa7dfa59947e6ec23814885bc9db5
David Wasserman <davidphilipwasserman@gmail.com>
2021-03-01 18:01:45 -0500

    Added HasBsmt feature

--- ames/data_prep.py
+++ ames/data_prep.py
@@ -45,6 +45,7 @@ def add_features(housing, save_to_disk=True, output_location=config.HOUSING_PICK
     housing['UnusedLotSize'] = housing['LotArea'] - housing['1stFlrSF']
 
     housing['HasPool'] = (housing['PoolArea']>0)
+    housing['HasBsmt'] = np.where(housing['BsmtQual']=='None',0,1)
 
     housing['Toilets'] = housing['HalfBath'] + housing['FullBath'] + housing['BsmtFullBath'] + housing['BsmtHalfBath']
     housing['Showers'] = housing['FullBath'] + housing['BsmtFullBath']
